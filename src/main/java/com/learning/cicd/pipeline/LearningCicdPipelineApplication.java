bf8087caf9bf8eec225881e11b2f3e36b6023072
Kishan Kushwaha <kishankushwaha076@gmail.com>
2026-01-11 18:18:16 +0530

    tomcat 9 ke feasiability hamne code me changes kiya hai

--- src/main/java/com/learning/cicd/pipeline/LearningCicdPipelineApplication.java
+++ src/main/java/com/learning/cicd/pipeline/LearningCicdPipelineApplication.java
@@ -2,9 +2,16 @@ package com.learning.cicd.pipeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LearningCicdPipelineApplication {
+public class LearningCicdPipelineApplication extends SpringBootServletInitializer{
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LearningCicdPipelineApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningCicdPipelineApplication.class, args);
--- src/main/java/com/learning/cicd/pipeline/controller/TestController.java
+++ src/main/java/com/learning/cicd/pipeline/controller/TestController.java
@@ -1,14 +1,16 @@
 package com.learning.cicd.pipeline.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 @RequestMapping("/test")
 public class TestController {
 
-	@GetMapping
+	
+	@RequestMapping
+	@ResponseBody
 	public String test() {
 		return "Hello Kishan";
 	}
