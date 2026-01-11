5c510aa6f0dfa20b3e61e0192a6d72b8ddbc27f7
Kishan Kushwaha <kishankushwaha076@gmail.com>
2026-01-11 14:52:57 +0530

    t

---
+++ src/main/java/com/learning/cicd/pipeline/LearningCicdPipelineApplication.java
@@ -0,0 +1,14 @@
+package com.learning.cicd.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningCicdPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningCicdPipelineApplication.class, args);
+		System.out.println("Hello 1");
+	}
+
+}
---
+++ src/test/java/com/learning/cicd/pipeline/LearningCicdPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.cicd.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningCicdPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
