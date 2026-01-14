e020dfdbe6125033b17bd93afb41557aec9bb796
Kishan Kushwaha <kishankushwaha076@gmail.com>
2026-01-14 16:00:50 +0530

    creating pipeline

---
+++ src/main/java/com/learning/cicd/pipeline/creating/pipeline/Test.java
@@ -0,0 +1,9 @@
+package com.learning.cicd.pipeline.creating.pipeline;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Learn cretaing pipeline using Jenkins and GIT");
+	}
+	
+}
